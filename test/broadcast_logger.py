72611bb78415406c549965ff07fd349248467952
k.snoek <k.snoek@bronkhorst.com>
2020-09-09 15:51:05 +0200

    Update logger test

--- test/broadcast_logger.py
+++ test/broadcast_logger.py
@@ -1,12 +1,11 @@
 import propar
 import time
-import random 
 from datetime import datetime
 
-dut = propar.instrument('com5')
+dut = propar.instrument('com1')
 
 # Get log file name with timestamp
-dump_file_name = datetime.now().strftime('%Y%m%d_%H%M%S_MEMS_Dump.csv')
+dump_file_name = datetime.now().strftime('%Y%m%d_%H%M%S_Dump.csv')
 
 # Callback function for receiving the broadcast dump data
 first = True
@@ -24,20 +23,7 @@ def broadcast_receive(parms):
 # Set the callback to log broadcast data
 dut.master.broadcast_callback = broadcast_receive
 
-# Start the dump / broadcast
-dut.writeParameter(7, 64)
-dut.writeParameter(328, 460800)
-dut.master.set_baudrate(460800)
-time.sleep(1)
-dut.writeParameter(104, 1)
-
-# get parameter object for setpoint, we need this to call write_parameters, where we can write without ack
-parm_setpoint = dut.db.get_parameter(9)
-
 # Main scipt.
 while True:
   time.sleep(1)  
-  # write new setpoint without ack
-  parm_setpoint['data'] = random.randint(0, 32001)
-  dut.write_parameters([parm_setpoint], command=propar.PP_COMMAND_SEND_PARM)  
 
